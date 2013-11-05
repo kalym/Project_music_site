240af4758e10eccd83c0a4c56457b9e5f477fcbc
Pavel <pavel.nykytiuk@engagepoint.com>
2013-11-05 10:24:13 +0200

    FIX: titles were fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 //twitter's button script
-<script>!function (d, s, id) {
+!function (d, s, id) {
     var js, fjs = d.getElementsByTagName(s)[0], p = /^http:/.test(d.location) ? 'http' : 'https';
     if (!d.getElementById(id)) {
     js = d.createElement(s);
@@ -7,7 +7,7 @@
     js.src = p + '://platform.twitter.com/widgets.js';
     fjs.parentNode.insertBefore(js, fjs);
     }
-}(document, 'script', 'twitter-wjs');</script>
+}(document, 'script', 'twitter-wjs');
 
 //start carousel
 $('document').ready(function () {
