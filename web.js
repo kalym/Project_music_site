0f93b3cb54c296558f2a756623e3802073cde40d
Pavel <pavel.nykytiuk@engagepoint.com>
2013-11-13 13:27:12 +0200

    ADD: package.json was added

--- web.js
+++ web.js
@@ -1,26 +1,15 @@
-var fs = require('fs');
-var buffer = fs.readFileSync('index.html');
-var buffer2 = fs.readFileSync('contacts.html');
+var http=require('http');
+var url=require('url');
 
+var server=http.createServer(function(req,res){
+    var pathname=url.parse(req.url).pathname;
+    switch(pathname){
+        case '/contacts':
+            res.end('subpage');
+            break;
+        default:
+            res.end('default');
+            break;
+    }
 
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-    response.send(buffer.toString());
-});
-app.get('/contacts', function(request, response) {
-    response.send(buffer2.toString());
-});
-
-var port = process.env.PORT || 8080;
-app.listen(port, function() {
-    console.log("Listening on " + port);
-});/**
- * Created with JetBrains WebStorm.
- * User: pavel.nykytiuk
- * Date: 13.11.13
- * Time: 12:35
- * To change this template use File | Settings | File Templates.
- */
+}).listen(8080);
